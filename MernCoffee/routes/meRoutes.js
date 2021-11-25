d7106df3f0696befb6de904aa26d282e366d3c2c
LeVanDong <itlvd@outlook.com>
2021-11-25 14:20:14 +0700

    Billing - add to cart
    Dong - Dong ba
    - Add bill routes
    - Add bill page
    - Add checkout page
    - Add view bill adminOrderRoutes

--- MernCoffee/routes/meRoutes.js
+++ MernCoffee/routes/meRoutes.js
@@ -12,48 +12,48 @@ const User = require('../models/userModel');
 /*
  * GET profile
  */
-router.get('/', isUser, async function (req, res) {
-  const user = req.user;
-  user.password = undefined;
-  user.role = undefined;
-  user.__v = undefined;
-  user.cart = undefined;
+router.get('/', isUser, async function(req, res) {
+    const user = req.user;
+    user.password = undefined;
+    user.role = undefined;
+    user.__v = undefined;
+    user.cart = undefined;
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user
-    }
-  })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    })
 });
 
 /*
  * POST profile
-*/
-router.post('/', isUser, async function (req, res) {
-  const user = await User.findByIdAndUpdate(req.user, req.body, {
-    new: true,
-    runValidators: true,
-  });
+ */
+router.post('/', isUser, async function(req, res) {
+    const user = await User.findByIdAndUpdate(req.user, req.body, {
+        new: true,
+        runValidators: true,
+    });
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user
-    }
-  });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    });
 });
 
 /*
  * DELETE account
-*/
-router.delete('/', isUser, async function (req, res) {
-  const user = await User.findByIdAndDelete(req.user);
+ */
+router.delete('/', isUser, async function(req, res) {
+    const user = await User.findByIdAndDelete(req.user);
 
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
 });
 
 module.exports = router;
