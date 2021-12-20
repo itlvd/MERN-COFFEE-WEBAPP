ac9440af7dea6f27067b2eae84e7dd2767f8958f
thientoan0101 <thientoancubi@gmail.com>
2021-12-20 11:15:13 +0700

    update image from cloud

--- MernCoffee/component/product/productService.js
+++ MernCoffee/component/product/productService.js
@@ -11,3 +11,36 @@ exports.findProduct = async(obj) => {
 exports.findByCategory = async(category) => {
   return await Product.find({ category: category })
 }
+
+
+exports.findProductWithSlug = async (slug) => {
+  const pro = await Product.findOne({slug: slug});
+  return pro;
+}
+
+exports.findProductWithId = async (id) => {
+  const pro = await Product.findById({_id: id});
+  return pro;
+}
+
+exports.createNewProduct = async (pro) => {
+  const product = await new Product(pro);
+  
+  await product.save();
+  return product
+}
+
+exports.updateImage = async (newLink, id) => {
+  const product = await Product.findOne({
+      _id: id
+  });
+  if (product) {
+      product.image = newLink;
+      await product.save();
+  }
+}
+
+exports.updateProduct = async (id, fields) => {
+    await Product.findByIdAndUpdate(id, fields);
+
+}
